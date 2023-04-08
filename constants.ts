eb58ee63563d39603a1d8a38fc1bce9df488fada
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-08 16:54:27 +0900

    docs(constants): add code comment

--- constants.ts
+++ constants.ts
@@ -1,15 +1,28 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-/** Cross-origin embedded policy value. */
+/** Cross-origin embedded policy value.
+ * @see [embedder policy value](https://html.spec.whatwg.org/multipage/browsers.html#embedder-policy-value)
+ */
 export enum EmbedderPolicyValue {
+  /** Cross-origin resources can be fetched without giving explicit permission through the [CORS protocol](https://fetch.spec.whatwg.org/#http-cors-protocol) or the [`Cross-Origin-Resource-Policy`](https://fetch.spec.whatwg.org/#http-cross-origin-resource-policy) header.
+   * @see ["unsafe-none"](https://html.spec.whatwg.org/multipage/browsers.html#coep-unsafe-none)
+   */
   UnsafeNone = "unsafe-none",
+
+  /** Fetching cross-origin resources requires the server's explicit permission through the [CORS protocol](https://fetch.spec.whatwg.org/#http-cors-protocol) or the [`Cross-Origin-Resource-Policy`](https://fetch.spec.whatwg.org/#http-cross-origin-resource-policy) header.
+   * @see ["require-corp"](https://html.spec.whatwg.org/multipage/browsers.html#coep-require-corp)
+   */
   RequireCorp = "require-corp",
+
+  /** Fetching cross-origin no-CORS resources omits credentials.
+   * @see ["credentialless"](https://html.spec.whatwg.org/multipage/browsers.html#coep-credentialless)
+   */
   Credentialless = "credentialless",
 }
 
 export const enum PolicyHeader {
   CrossOriginEmbeddedPolicy = "cross-origin-embedded-policy",
   CrossOriginEmbeddedPolicyReportOnly =
-    `cross-origin-embedded-policy-report-only`,
+    `${PolicyHeader.CrossOriginEmbeddedPolicy}-report-only`,
 }
